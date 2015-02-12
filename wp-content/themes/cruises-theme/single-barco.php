e006f829ac3601477064b42aa5c24c42294a2086
Pablo Calvo <pcalvo@konradgroup.com>
2015-02-12 09:07:26 -0600

    Fixed duplicated banner on single barco

--- wp-content/themes/cruises-theme/single-barco.php
+++ wp-content/themes/cruises-theme/single-barco.php
@@ -111,6 +111,5 @@
 </div>
     
 <?php get_template_part( 'partials/-noticias', 'page' ); ?>  
-<?php get_template_part( 'partials/-reserve', 'page' ); ?>
 <?php wp_footer(); // Crucial footer hook! ?>
 <?php get_footer(); ?>
